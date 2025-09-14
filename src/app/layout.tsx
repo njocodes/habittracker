83f720e9f27c47303f04d18f9c128885007d682b
Jasper Daugart <jasperdaugart@gmail.com>
2025-09-14 23:20:57 +0200

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SessionProvider } from "@/components/SessionProvider";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <SessionProvider>
           {children}
+          <Analytics />
         </SessionProvider>
       </body>
     </html>
